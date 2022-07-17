903b9bc2b5c192086577a2b4a47753c1f958ac9a
afz123 <asimovafzalhon@yandex.ru>
2022-07-17 12:54:35 +0300

    lil change

--- Remove Element.py
+++ Remove Element.py
@@ -1,5 +1,5 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums: list, val: int) -> int:
         for i,j in enumerate(sorted(nums)): 
             if j==val: nums.remove(j)
         return len(nums)
